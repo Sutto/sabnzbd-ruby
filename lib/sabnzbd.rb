cea7d86dc1368d6edadf1577daf14445453dbfb5
Darcy Laycock <sutto@sutto.net>
2009-01-08 16:02:14 +0900

    Add basic ini parser, SABnzbd.local to attempt to login using the local settings

--- lib/sabnzbd.rb
+++ lib/sabnzbd.rb
@@ -121,4 +121,63 @@ class SABnzbd
     Status.jobs_from(status)
   end
   
+  class << self
+    
+    @@settings_path = File.expand_path("~/.sabnzbd/sabnzbd.ini")
+    
+    def settings_path
+      @@settings_path
+    end
+    
+    def settings_path=(value)
+      @@settings_path = value
+    end
+    
+    def settings
+      self.parse_ini(File.read(self.settings_path))
+    end
+    
+    def local
+      s = self.settings
+      self.new(s["misc"]["username"], s["misc"]["password"])
+    end
+    
+    protected
+    
+    def parse_ini(text)
+      options = {}
+      keys = []
+      text.each_line do |line|
+        if line =~ /^(\[+([^\[\]]+)\]+)$/
+          normal, name = $1, $2
+          start = line.match(/^\[+/)[0]
+          depth = start.length
+          if depth == 1
+            keys = []
+          elsif depth <= keys.size
+            width = (depth - 2)
+            width = 0 if width < 0
+            keys = keys[0..width]
+          end
+          keys << name
+          set_value options, keys, {}
+        elsif line =~ /^(.*)=(.*)$/
+          key, value = $1.strip, $2.strip
+          set_value options, (keys + [key]), value
+        end
+      end
+      return options
+    end
+    
+    def set_value(options, keys, value)
+      r = options
+      keys[0..-2].each do |k|
+        r = (r[k] ||= {})
+      end
+      r[keys.last] = value
+      return options
+    end
+    
+  end
+  
 end
